5c117678119c7bb0d4955747fc04bdaa74b3178c
HomHom1 <homahomen2@gmail.com>
2024-12-19 08:27:54 -0600

    Day 7

---
+++ 2024/7/7b.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool backtrack(vector<int> nums, int pos, long long target, long long currentValue)
+{
+    if (pos == nums.size() - 1)
+    {
+        return currentValue == target;
+    }
+
+    if (backtrack(nums, pos + 1, target, currentValue + nums[pos + 1]))
+    {
+        return true;
+    }
+
+    if (backtrack(nums, pos + 1, target, currentValue * nums[pos + 1]))
+    {
+        return true;
+    }
+
+    string concat = to_string(currentValue) + to_string(nums[pos+1]);
+    if (backtrack(nums, pos+1, target, stoll(concat))) {
+        return true;
+    }
+
+    return false;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cout << "Missing input text file";
+        return 0;
+    }
+    ifstream file(argv[1]);
+
+    if (!file.is_open())
+    {
+        cerr << "There was an error opening file";
+        return 1;
+    }
+
+    long long total = 0;
+
+    string line;
+    while (getline(file, line))
+    {
+        size_t colonPos = line.find(':');
+        long long target = stoll(line.substr(0, colonPos));
+
+        stringstream s(line.substr(colonPos + 1));
+        int num;
+        vector<int> nums;
+        while (s >> num)
+        {
+            nums.push_back(num);
+        }
+
+        if (backtrack(nums, 0, target, nums[0]))
+        {
+            total += target;
+        }
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
